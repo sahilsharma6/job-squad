075553eecfaa717b3a0e083b387ae8b102b484ae
Arpit Sarkar <arpitsarkar16@gmail.com>
2025-01-17 20:42:12 +0530

    fix conflicts

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react"; 
-import HeroImg1 from "../../assets/Hero1.jpg";
+import HeroImg1 from "../../assets/hero1.jpg";
 
 const Hero = () => {
   return (
