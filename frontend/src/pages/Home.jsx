54d45dc501d17e572c36abc974f40aae129ebe05
Pranaymaurya <139427478+Pranaymaurya@users.noreply.github.com>
2025-01-17 19:19:25 +0530

    components made

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import JobMatchHero from "@/Home/Banner1";
+import HiringBanner from "@/Home/Hiring";
+import ExploreMarketplace from "@/Home/Marketplace";
+import NewsletterSection from "@/Home/Newletter";
+import TopRecruiters from "@/Home/Recrut";
+
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <>
+    
+    <TopRecruiters/>
+    <HiringBanner/>
+    <ExploreMarketplace/>
+    <NewsletterSection/>
+    <JobMatchHero/>
+    </>
+  );
+
 };
 
 export default HomePage;
