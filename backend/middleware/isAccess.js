bb0e81f428889e4e768b9776ce8157a66ce33cf8
Rudra-Maity <maityrudra228@gmail.com>
2025-01-18 18:18:19 +0530

    added auth middlewares

---
+++ backend/middleware/isAccess.js
@@ -0,0 +1,9 @@
+export const isAccess=(roles)=>{
+    return (req,res,next)=>{
+        const userData=req.user.data;
+        if (!roles.includes(userData.role)) {
+            return res.status(403).json({ message: "Access denied. You do not have the required permissions." });
+        }
+        next()
+        }
+}
