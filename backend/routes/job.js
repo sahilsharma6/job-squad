9cff522436a76b1ef73d2caac47d782ba2525f5f
Rudra-Maity <maityrudra228@gmail.com>
2025-01-21 19:58:09 +0530

    updated application and job

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -1,20 +1,20 @@
 import { Router } from "express";
 import { getAllJobs,createJob,getJobById,updateJob,deleteJob, getJobByFilter, getJobSectors, getJobSectorById, createJobSector } from "../controllers/Job.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
-import { isAccess } from "../middleware/isAccess.js";
+import { AccessRole } from "../middleware/AccessRole.js";
 
 const JobRoutes = Router();
 
 JobRoutes.get("/all",getAllJobs);
 JobRoutes.get("/jobbyfilters",getJobByFilter);
-JobRoutes.get("/:jobId",isAuthenticated,isAccess(['candidate','company','admin']) ,getJobById);
-JobRoutes.post("/create",isAuthenticated,isAccess(['company','admin']) ,createJob);
-JobRoutes.put("/update/:jobId",isAuthenticated,isAccess(['company','admin']) ,updateJob);
-JobRoutes.delete("/delete/:jobId",isAuthenticated,isAccess(['company','admin']) ,deleteJob);
+JobRoutes.get("/:jobId",isAuthenticated,AccessRole(['candidate','company','admin']) ,getJobById);
+JobRoutes.post("/create",isAuthenticated,AccessRole(['company','admin']) ,createJob);
+JobRoutes.put("/update/:jobId",isAuthenticated,AccessRole(['company','admin']) ,updateJob);
+JobRoutes.delete("/delete/:jobId",isAuthenticated,AccessRole(['company','admin']) ,deleteJob);
 
 //get Job Sectors
 JobRoutes.get('/jobsectors',getJobSectors);
 JobRoutes.get('/jobsector/:sectorId',getJobSectorById);
-JobRoutes.post('/create-job-sector',isAuthenticated,isAccess(['admin']),createJobSector);
+JobRoutes.post('/create-job-sector',isAuthenticated,AccessRole(['admin']),createJobSector);
 
 export default JobRoutes;
